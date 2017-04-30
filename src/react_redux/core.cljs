bdde5d89b12bb4adb6374c129f658c9c11ee1ff7
Larry Jones <larryj@blinds.com>
2017-04-29 20:13:52 -0500

    Initial page layout with components:
    * Home "page"
    * About "page"
    * "Header"

--- src/react_redux/core.cljs
+++ src/react_redux/core.cljs
@@ -5,13 +5,25 @@
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div {:class "jumbotron"} [:h1 "Welcome to Reagent"]
+   [:p "React in ClojureScript for ultra-responsive web apps."]
+   [:a {:href "/about" :class "btn btn-primary btn-lg"} "Learn More"]])
+
+(defn about-page []
+  [:div [:h1 "About"]
+   [:p "This application uses ClojureScript, Secretary and other helpful libraries."]])
+
+(defn app []
+  [:div {:class "container-fluid"}
+   [:p "Header here..."]
+   (home-page)
+   (about-page)])
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (reagent/render [home-page] (.getElementById js/document "app")))
+  (reagent/render [app] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
